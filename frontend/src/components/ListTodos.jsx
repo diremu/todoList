27e3ef757297fbd835c29b990482e00524528bbc
Diremu Adebanjo <diremuadebanjo@gmail.com>
2024-07-24 11:57:57 +0100

    changes to the list todos styling

--- frontend/src/components/ListTodos.jsx
+++ frontend/src/components/ListTodos.jsx
@@ -1,40 +1,47 @@
 import { useState, useEffect } from "react"
 
-const ListTodos = async () => {
+const ListTodos = () => {
     const [todos, setTodos] = useState([])
-
-    const getTodos = async () => {
-        try {
-            const fetched = await fetch("http://localhost:5000/todos")
-            const body = await fetched.json()
-            setTodos(body)
-        } catch (err) {
-            console.error(err.message)
-        }
-    }
-    useEffect(() => { getTodos() }, [])
-    console.log(todos)
+    useEffect(
+        () => {
+            const getTodos = async () => {
+                try {
+                    const fetched = await fetch("http://localhost:5000/todos")
+                    const body = await fetched.json()
+                    setTodos(body)
+                } catch (err) {
+                    console.error(err.message)
+                }
+            };
+            getTodos()
+        }, []
+    )
 
     return (
         <>
-            <div>
-                <table>
+            <div className="border-gray-200 border-2">
+                <table className="table-fixed w-full px-4">
                     <thead>
                         <tr>
-                            <th>Number</th>
-                            <th>Description</th>
-                            <th>Edit</th>
+                            <th className="w-[10vw]">S/N</th>
+                            <th className="w-[90vw]">Description</th>
                         </tr>
                     </thead>
                     <tbody>
-                        {todos.map(
+                        {todos.length > 0 ? todos.map(
                             (todo) => (
-                                <tr key={todo.todo_id}>
-                                    <td>{todo.description}</td>
-                                    <td>Edit</td>
-                                    <td>Delete</td>
+                                <tr key={todo.todo_id} className=" bg-yellow-400  rounded-xl my-4">
+                                    <td className="text-xl text-center">{todo.todo_id}</td>
+                                    <td className="flex justify-between items-center mr-6">
+                                    <span className="text-xl">{todo.description}</span>
+                                    <div className=" inline-flex space-x-2">
+                                        <button>&#10003;</button>
+                                        <button>&#10005;</button>
+                                    </div>
+
+                                    </td>
                                 </tr>
-                            ))}
+                            )) : <tr><td colSpan="3">No todos</td></tr>}
                     </tbody>
                 </table>
             </div>
