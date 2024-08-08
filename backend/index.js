1b20843501c2d2273f448bf40386f1c0799925fc
Diremu Adebanjo <diremuadebanjo@gmail.com>
2024-08-08 12:02:19 +0100

    stopped temporarily

--- backend/index.js
+++ backend/index.js
@@ -17,17 +17,34 @@ app.get("/todos", async (req, res) => {
 });
 
 //to create a todo
-app.post("/todos", async (req,res) => {
+app.post("/todos", async (req, res) => {
   try {
-    const {description, id} = req.body;
-    const newTodo = await pool.query("INSERT INTO todo(todo_id, description) VALUES($1,$2) RETURNING *",[id, description])
-    res.status(200).json(newTodo.rows)
-    console.log(description, id)
+    const { description, id } = req.body;
+    const newTodo = await pool.query(
+      "INSERT INTO todo(todo_id, description) VALUES($1,$2) RETURNING *",
+      [id, description]
+    );
+    res.status(200).json(newTodo.rows);
+    console.log(description, id);
   } catch (err) {
-    console.error(err.message)
-    res.status(500).send("Server error with posting data. Try again")
+    console.error(err.message);
+    res.status(500).send("Server error with posting data. Try again");
   }
-})
+});
+
+app.put("/todos", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { description } = req.body;
+    const updatedTodo = await pool.query(
+      "UPDATE todo SET description = $1 WHERE todo_id = $2",
+      [description, id]
+    );
+    res.status(200).json("Todo was updated");
+  } catch (error) {
+    console.error(error.message)
+  }
+});
 
 app.listen(5000, () => {
   console.log("Connected to port 5000");
